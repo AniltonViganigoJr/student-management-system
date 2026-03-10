aec44ee6aaaefedba72f5670ab58465137779ec4
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2026-03-09 23:11:53 -0300

    feat: implement RegraDeNegocioException

---
+++ src/main/java/edu/myschoolapp/aluno/exceptions/RegraDeNegocioException.java
@@ -0,0 +1,8 @@
+package edu.myschoolapp.aluno.exceptions;
+
+public class RegraDeNegocioException extends RuntimeException {
+
+    public RegraDeNegocioException(String mensagem) {
+        super(mensagem);
+    }
+}
